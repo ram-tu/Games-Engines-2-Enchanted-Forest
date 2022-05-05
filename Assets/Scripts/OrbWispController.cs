5a1f535245e0bf7c6fa1a757abb79a354b39cca0
ram-tu <46999658+ram-tu@users.noreply.github.com>
2022-05-05 10:33:24 +0100

    rotate around orb

--- Assets/Scripts/OrbWispController.cs
+++ Assets/Scripts/OrbWispController.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class OrbWispController : MonoBehaviour
 {
     public GameObject target;
+    public float angle;
+    public bool isSideways;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,6 +16,10 @@ public class OrbWispController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.RotateAround(target.transform.position, Vector3.up, 20 * Time.deltaTime);
+        if(isSideways)
+            transform.RotateAround(target.transform.position, Vector3.up, angle * Time.deltaTime);
+        else
+        {
+            transform.RotateAround(target.transform.position, Vector3.right, angle * Time.deltaTime);        }
     }
 }
